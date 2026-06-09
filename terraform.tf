ec7bce58a310e9dfcc9ae85b78df7822d70be9c7
Luis Angel Pari Tesillo <lpari@osce.gob.pe>
2026-06-09 09:17:10 -0500

    Cambio de apellido

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 locals {
-  idapp = "apellido" # Apellido
+  idapp = "pari" # Apellido
 }
 
 terraform {
@@ -13,5 +13,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "b6dd1983-a328-4ef9-aae9-b401b616479d" # Id de suscripción
+  subscription_id = "1043272f-49a4-48b9-8a42-18a70413117f" # Id de suscripción
 }
